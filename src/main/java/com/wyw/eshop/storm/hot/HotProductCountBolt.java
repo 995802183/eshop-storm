e73275c7c438a6071f193d63ec8dc911bed5d081
wangyongwen <995802183@qq.com>
2019-08-09 14:37:41 +0800

    modify

--- src/main/java/com/wyw/eshop/storm/hot/HotProductCountBolt.java
+++ src/main/java/com/wyw/eshop/storm/hot/HotProductCountBolt.java
@@ -124,4 +124,77 @@ public class HotProductCountBolt extends BaseRichBolt {
             }
         }
     }
+
+    private class HotProductFindThread implements Runnable{
+
+        @Override
+        public void run() {
+            List<Map.Entry<Long,Long>> productCountList = new ArrayList<>();
+            List<Long> hotProductIdList = new ArrayList<>();
+            while (true){
+                // 1、将LRUMap中的数据按照访问次数，进行全局的排序
+                // 2、计算95%的商品的访问次数的平均值
+                // 3、遍历排序后的商品访问次数，从最大的开始
+                // 4、如果某个商品比如它的访问量是平均值的10倍，就认为是缓存的热点
+                try {
+                    productCountList.clear();
+                    hotProductIdList.clear();
+                    if(productCountMap.size() == 0){
+                        Utils.sleep(100);
+                        continue;
+                    }
+
+                    //1先做全局的排序
+                    for(Map.Entry<Long,Long> productCountEntry: productCountMap.entrySet()) {
+                        if(productCountList.size() == 0){
+                            productCountList.add(productCountEntry);
+                        }else{
+                            Boolean bigger = false;
+                            for(Integer i = 0;i<productCountList.size();i++){
+                                Map.Entry<Long, Long> topnProductCountEntry = productCountList.get(i);
+                                if(productCountEntry.getValue() > topnProductCountEntry.getValue()){
+                                    int lastIndex = productCountList.size() < productCountMap.size()? productCountList.size()-1:productCountMap.size()-2;
+                                    for(int j=lastIndex;j>-i;j--){
+                                        if(j+1 == productCountList.size()){
+                                            productCountList.add(null);
+                                        }
+                                        productCountList.set(i,productCountEntry);
+                                        bigger = true;
+                                        break;
+                                    }
+                                }
+                            }
+                            if(!bigger){
+                                if(productCountList.size() < productCountMap.size()){
+                                    productCountList.add(productCountEntry);
+                                }
+                            }
+                        }
+                    }
+
+                    // 2、计算出95%的商品的访问次数的平均值
+                    int calculateCount = (int)Math.floor(productCountList.size() * 0.95);
+                    Long totalCount = 0L;
+                    for(int i = productCountList.size()-1;i>=productCountList.size()-calculateCount;i--){
+                        totalCount += productCountList.get(i).getValue();
+                    }
+                    Long avgCount = totalCount / calculateCount;
+                    // 3、从第一个元素开始遍历，判断是否是平均值得10倍
+                    for(Map.Entry<Long,Long> productCountEntry: productCountList){
+                        if(productCountEntry.getValue() > 10 * avgCount){
+                            hotProductIdList.add(productCountEntry.getKey());
+
+
+                            //将缓存热点反向推送到流量分发的nginx中
+
+                            //将缓存热点，那个商品对应的完整的缓存数据，发送请求到缓存服务去获取，反向推送到所有的后端应用nginx服务器上去
+                        }
+                    }
+                    Utils.sleep(5000);
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
 }
